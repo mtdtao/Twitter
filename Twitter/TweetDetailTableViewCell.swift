44a66a4e2c1cf9d30870be1a74e93c7a1789dc8e
mtdtao <mtdtao@ZengJintaos-MacBook-Pro.local>
2016-02-21 21:23:19 -0500

    add more function

---
+++ Twitter/TweetDetailTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetDetailTableViewCell.swift
+//  Twitter
+//
+//  Created by ZengJintao on 2/16/16.
+//  Copyright © 2016 ZengJintao. All rights reserved.
+//
+
+import UIKit
+
+class TweetDetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Twitter/UserInfoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserInfoTableViewCell.swift
+//  Twitter
+//
+//  Created by ZengJintao on 2/19/16.
+//  Copyright © 2016 ZengJintao. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
